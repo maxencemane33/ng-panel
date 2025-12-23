1e9a7516b964d5794bf046e2f51fd133ba1ea958
maxencemane33 <maxencemane@gmail.com>
2025-12-23 18:27:26 +0100

    correction

--- worker/src/index.js
+++ worker/src/index.js
@@ -1,28 +1,7 @@
 export default {
-  async fetch(request, env) {
-
-    const url = new URL(request.url);
-
-    if (url.pathname !== "/alliance") {
-      return new Response("Not found", { status: 404 });
-    }
-
-    const ngRes = await fetch(
-      "https://publicapi.nationsglory.fr",
-      {
-        headers: {
-          "Authorization": `Bearer ${env.NG_API_KEY}`
-        }
-      }
-    );
-
-    const data = await ngRes.json();
-
-    return new Response(JSON.stringify(data), {
-      headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*"
-      }
+  async fetch(req, env) {
+    return new Response(JSON.stringify({ message: "Worker actif !" }), {
+      headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" }
     });
   }
-};
+}
